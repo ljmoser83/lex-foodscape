f86b88bfb1c24bf3347e841bcf69565fe7abb75a
Moser <ljmose2@uky.edu>
2019-04-10 15:01:37 -0400

    integrated more assembly.css styles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(function () {
-    var mymap = L.map('map').setView([38.03962, -84.496257], 12);
+    var mymap = L.map('map', {
+        zoomControl:true
+    }).setView([38.03962, -84.496257], 12);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
