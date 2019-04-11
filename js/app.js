2ac132d141ebe5739dd6692a49125f28ca057413
Moser <ljmose2@uky.edu>
2019-04-11 13:07:20 -0400

    got map functioning reasonably well

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-$(function () {
+(function () {
     var mymap = L.map('map', {
-        zoomControl:true
+        zoomControl: true
     }).setView([38.03962, -84.496257], 12);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoibGptb3NlcjgzIiwiYSI6ImNqMG5sNmI1bjAwY3UzM3Q4cXNncGl6NDMifQ.jAX66dC8oy8Mh4IvgF5SPg'
@@ -76,20 +76,26 @@ $(function () {
 
     // add the control to the map
     geocodeControl.addTo(mymap);
-
-    $.when($.getJSON('data/C/C.json'),
-        $.getJSON('data/F/F.json'),
-        $.getJSON('data/G/G.json'),
-        $.getJSON('data/L/L.json')
-    ).done(function (C, F, G, L) {
-        console.log(C);
-        console.log(F);
-        console.log(G);
-        console.log(L);
+    $.when($.getJSON('data/Lex_Food/lf.json')).done(function (lf) {
 
 
+        var food = L.markerClusterGroup();
+
+        // loop through all our signals features
+        lf.features.forEach(function (feature) {
+            // create a new Leaflet marker for each
+            var coords = feature.geometry.coordinates,
+                marker = L.marker([coords[1], coords[0]]);
+            // bind a tooltip to the marker
+            marker.bindTooltip("Name: " + feature.properties.Name);
+            // add the marker to the markerClusterGroup
+            food.addLayer(marker);
+
+        });
+        // add the markerClusterGroup to the map
+        mymap.addLayer(food);
+    });
 
 
 
-    })
-})
+})()
