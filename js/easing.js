7207ba5b57b03cbda8a445d1fd3201d6a3a4fbf0
Lee Moser <lee.moser@uky.edu>
2019-04-20 10:42:04 -0400

    applied fix to console.log error

--- js/easing.js
+++ js/easing.js
@@ -1,4 +1,20 @@
 (function (factory) {
+    var methods = ["assert", "cd", "clear", "count", "countReset",
+    "debug", "dir", "dirxml", "error", "exception", "group", "groupCollapsed",
+    "groupEnd", "info", "log", "markTimeline", "profile", "profileEnd",
+    "select", "table", "time", "timeEnd", "timeStamp", "timeline",
+    "timelineEnd", "trace", "warn"];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+    var method;
+    var noop = function () {};
+    while (length--) {
+        method = methods[length];
+        // define undefined methods as noops to prevent errors
+        if (!console[method])
+            console[method] = noop;
+    }
+
     if (typeof define === "function" && define.amd) {
         define(["jquery"], function ($) {
             return factory($)
